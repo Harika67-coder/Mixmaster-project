494b2c71abc99ca3f0c0af7b9612f647ae196884
Harika-67-coder <abhi8341508426@gmail.com>
2024-11-27 08:29:46 +0530

    first commit

---
+++ src/assets/wrappers/Newsletter.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+const Wrapper = styled.form`{
+   margin:10px;
+   margin-top:80px;
+   background:white;
+   width:80vw;
+   max-width:1120px;
+   height:auto;
+   overflow-y:auto;
+   padding:30px 40px;
+   box-shadow:3px 3px 5px lightgrey;
+}`
+export default Wrapper;
